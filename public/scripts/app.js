bd771b64fd0e5f8672a18b9f237956673e34b152
James Finkelstein <jfinkelstein9@me.com>
2018-09-07 16:32:14 -0400

    classes refresher added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,38 +1,17 @@
 'use strict';
 
-var details = {
-    title: 'Hey! these are some details you can now see!!!',
-    toggle: false
-};
-
-var toggleVisibility = function toggleVisibility(e) {
-    e.preventDefault();
-    details.toggle = !details.toggle;
-    render();
-};
-
 var render = function render() {
-    var JSX = React.createElement(
+    var temp = React.createElement(
         'div',
         null,
         React.createElement(
-            'h1',
-            null,
-            'Visibility Toggle'
-        ),
-        React.createElement(
-            'button',
-            { onClick: toggleVisibility },
-            details.toggle ? 'Hide Details' : 'Show Details'
-        ),
-        details.toggle && React.createElement(
             'p',
             null,
-            details.title
+            'hello'
         )
     );
 
-    ReactDOM.render(JSX, document.getElementById('app'));
+    ReactDOM.render(temp, document.getElementById('app'));
 };
 
 render();
---
+++ src/playground/es6-classes-1.js
@@ -0,0 +1,11 @@
+const render = () => {
+    const temp = (
+        <div>
+            <p>hello</p>
+        </div>
+    )
+
+    ReactDOM.render(temp, document.getElementById('app'));
+};
+
+render();
