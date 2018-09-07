0169fa1b616a107559d3ed6a3d3f021d9412b55f
James Finkelstein <jfinkelstein9@me.com>
2018-09-06 20:21:01 -0400

    regular vs shorthand arrow

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,28 @@
-"use strict";
+'use strict';
 
-var square = function square(x) {
+function square(x) {
+    return x * x;
+};
+
+var squareArrow = function squareArrow(x) {
     return x * x;
 };
 
 console.log(square(8));
+
+var fullName = 'James Finkelstein';
+
+// regular arrow function
+var getFirstName = function getFirstName(name) {
+    var firstName = name.split(' ')[0];
+    return firstName;
+};
+
+console.log(getFirstName(fullName));
+
+// shorthand arrow function
+var firstName = function firstName(name) {
+    return name.split(' ')[0];
+};
+
+console.log(firstName(fullName));
--- src/playground/es6-arrow-functions.js
+++ src/playground/es6-arrow-functions.js
@@ -1,7 +1,22 @@
-const square = function (x) {
+function square(x) {
     return x * x;
 };
 
-
+const squareArrow = (x) => x * x;
 
 console.log(square(8));
+
+const fullName = 'James Finkelstein';
+
+// regular arrow function
+const getFirstName = (name) => {
+    const firstName = name.split(' ')[0];
+    return firstName;
+};
+
+console.log(getFirstName(fullName));
+
+// shorthand arrow function
+const firstName = (name) => name.split(' ')[0];
+
+console.log(firstName(fullName));
