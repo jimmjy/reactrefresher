406b8b9b9c57a4587ca1a2a9518dc59d18933cae
James Finkelstein <jfinkelstein9@me.com>
2018-09-06 21:01:48 -0400

    added arrow function 2 exercises

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,28 +1,42 @@
 'use strict';
 
-function square(x) {
-    return x * x;
+// arguments object - no longer bound with arrow functions
+var add = function add(a, b) {
+    // console.log(arguments);
+    return a + b;
 };
 
-var squareArrow = function squareArrow(x) {
-    return x * x;
+console.log(add(55, 1));
+
+// this keyword - no longer bound
+
+var user = {
+    name: 'James',
+    cities: ['Toronto', 'Montreal', 'New York'],
+    printPlacesLived: function printPlacesLived() {
+        var _this = this;
+
+        return this.cities.map(function (city) {
+            return _this.name + ' has lived in ' + city;
+        });
+    }
 };
 
-console.log(square(8));
+console.log(user.printPlacesLived());
 
-var fullName = 'James Finkelstein';
+// challenge
 
-// regular arrow function
-var getFirstName = function getFirstName(name) {
-    var firstName = name.split(' ')[0];
-    return firstName;
+var multiplier = {
+    number: [1, 5, 9],
+    multiplyBy: 4,
+    // es6 method definition
+    multiply: function multiply() {
+        var _this2 = this;
+
+        return this.number.map(function (num) {
+            return num * _this2.multiplyBy;
+        });
+    }
 };
 
-console.log(getFirstName(fullName));
-
-// shorthand arrow function
-var firstName = function firstName(name) {
-    return name.split(' ')[0];
-};
-
-console.log(firstName(fullName));
+console.log(multiplier.multiply());
---
+++ src/playground/es6-arrow-function-2.js
@@ -0,0 +1,32 @@
+// arguments object - no longer bound with arrow functions
+const add = (a, b) => {
+    // console.log(arguments);
+    return a + b;
+};
+
+console.log(add(55, 1));
+
+// this keyword - no longer bound
+
+const user = {
+    name: 'James',
+    cities: ['Toronto', 'Montreal', 'New York'],
+    printPlacesLived() {
+        return this.cities.map((city) => this.name + ' has lived in ' + city);
+    }
+};
+
+console.log(user.printPlacesLived());
+
+// challenge
+
+const multiplier = {
+    number: [1, 5, 9],
+    multiplyBy: 4,
+    // es6 method definition
+    multiply() {
+        return this.number.map((num) => num * this.multiplyBy);
+    }
+};
+
+console.log(multiplier.multiply());
