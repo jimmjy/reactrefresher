b174f0dcf95eceabcaee80663ebca7ca8c4ce044
James Finkelstein <jfinkelstein9@me.com>
2018-09-07 15:37:11 -0400

    some events added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -31,6 +31,12 @@ var onRemoveAll = function onRemoveAll() {
     render();
 };
 
+var onMakeDecision = function onMakeDecision() {
+    var randomNum = Math.floor(Math.random() * app.options.length);
+    var option = app.options[randomNum];
+    alert(option);
+};
+
 var appRoot = document.getElementById('app');
 
 var render = function render() {
@@ -53,9 +59,9 @@ var render = function render() {
             app.options.length > 0 ? 'Here are your options' : 'No options'
         ),
         React.createElement(
-            'p',
-            null,
-            app.options.length
+            'button',
+            { disabled: app.options.length <= 0, onClick: onMakeDecision },
+            'What should I do?'
         ),
         React.createElement(
             'button',
--- src/app.js
+++ src/app.js
@@ -28,6 +28,12 @@ const onRemoveAll = () => {
     render();
 };
 
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNum];
+    alert(option);
+};
+
 const appRoot = document.getElementById('app');
 
 const render = () => {
@@ -36,7 +42,7 @@ const render = () => {
             <h1>{app.title}</h1>
             {app.subtitle && <p>{app.subtitle}</p>}
             <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-            <p>{app.options.length}</p>
+            <button disabled={app.options.length <= 0} onClick={onMakeDecision}>What should I do?</button>
             <button onClick={onRemoveAll}>Remove All</button>
             <ol>
                 {app.options.map((option) => <li key={option}>{option}</li>)}
