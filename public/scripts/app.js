e8464416fb4835172e1b9b7e2a384a9a4245b187
James Finkelstein <jfinkelstein9@me.com>
2018-09-09 16:06:14 -0400

    minor cleanups to exercises

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,17 +1,1 @@
-'use strict';
-
-var render = function render() {
-    var temp = React.createElement(
-        'div',
-        null,
-        React.createElement(
-            'p',
-            null,
-            'hello'
-        )
-    );
-
-    ReactDOM.render(temp, document.getElementById('app'));
-};
-
-render();
+"use strict";
