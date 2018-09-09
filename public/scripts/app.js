604ff53abdb2ae4fa57cd4bbfb748ab1103d8df4
James Finkelstein <jfinkelstein9@me.com>
2018-09-09 17:57:45 -0400

    playing with inheritance

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,12 @@
 'use strict';
 
+var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -33,10 +39,52 @@ var Person = function () {
 
 ;
 
-var me = new Person('James Finkelstein', 30);
-console.log(me.getGreeting());
+// sub classes using extends to bring attritbues of person class
+
+var Student = function (_Person) {
+    _inherits(Student, _Person);
+
+    function Student(name, age, major) {
+        _classCallCheck(this, Student);
+
+        // this allows us to take the default constructor from person without needed to actually set them up.
+        var _this = _possibleConstructorReturn(this, (Student.__proto__ || Object.getPrototypeOf(Student)).call(this, name, age));
+
+        _this.major = major;
+        return _this;
+    }
+
+    _createClass(Student, [{
+        key: 'hasMajor',
+        value: function hasMajor() {
+            return !!this.major;
+        }
+    }, {
+        key: 'getDescription',
+
+
+        // overrides the person method
+        value: function getDescription() {
+            var description = _get(Student.prototype.__proto__ || Object.getPrototypeOf(Student.prototype), 'getDescription', this).call(this);
+
+            if (this.hasMajor()) {
+                description += ' Their major is ' + this.major;
+            }
+            return description;
+        }
+    }]);
+
+    return Student;
+}(Person);
+
+;
+
+var me = new Student('James Finkelstein', 30, 'Computer Science');
+console.log(me);
+console.log(me.hasMajor());
 console.log(me.getDescription());
 
-var other = new Person();
-console.log(other.getGreeting());
+var other = new Student();
+console.log(other);
+console.log(other.hasMajor());
 console.log(other.getDescription());
--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -15,10 +15,34 @@ class Person {
 
 };
 
-const me = new Person('James Finkelstein', 30);
-console.log(me.getGreeting());
+// sub classes using extends to bring attritbues of person class
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age); // this allows us to take the default constructor from person without needed to actually set them up.
+        this.major = major;
+    };
+
+    hasMajor() {
+        return !!this.major;
+    };
+
+    // overrides the person method
+    getDescription() {
+        let description = super.getDescription();
+        
+        if (this.hasMajor()) {
+            description += ` Their major is ${this.major}`;
+        }
+        return description;
+    };
+};
+
+const me = new Student('James Finkelstein', 30, 'Computer Science');
+console.log(me);
+console.log(me.hasMajor());
 console.log(me.getDescription());
 
-const other = new Person();
-console.log(other.getGreeting());
+const other = new Student();
+console.log(other);
+console.log(other.hasMajor());
 console.log(other.getDescription());
