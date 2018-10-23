b5248d5b642e8dc6b7111810873f9d10c89247cb
James Finkelstein <jfinkelstein9@me.com>
2018-10-22 20:35:47 -0400

    updates to files

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,99 +1,15 @@
 'use strict';
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+//class
+var Person = function Person(name, age) {
+    _classCallCheck(this, Person);
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+    this.name = name;
+    this.age = age;
+};
 
-//converted build it to react method
-var VisibilityToggle = function (_React$Component) {
-    _inherits(VisibilityToggle, _React$Component);
+var me = new Person('James', 30);
 
-    function VisibilityToggle(props) {
-        _classCallCheck(this, VisibilityToggle);
-
-        var _this = _possibleConstructorReturn(this, (VisibilityToggle.__proto__ || Object.getPrototypeOf(VisibilityToggle)).call(this, props));
-
-        _this.state = {
-            toggle: false,
-            hide: 'Hide Details',
-            show: 'Show Details',
-            title: 'Hey! these are some details you can now see!!!!'
-        };
-        _this.handleToggleVisibility = _this.handleToggleVisibility.bind(_this);
-        return _this;
-    }
-
-    _createClass(VisibilityToggle, [{
-        key: 'handleToggleVisibility',
-
-
-        //class methods
-        value: function handleToggleVisibility() {
-            var newToggle = !this.state.toggle;
-            this.setState(function () {
-                return {
-                    toggle: newToggle
-                };
-            });
-        }
-    }, {
-        key: 'render',
-        value: function render() {
-            return React.createElement(
-                'div',
-                null,
-                React.createElement(
-                    'h1',
-                    null,
-                    'Visibility Toggle'
-                ),
-                React.createElement(
-                    'button',
-                    { onClick: this.handleToggleVisibility },
-                    this.state.toggle ? this.state.hide : this.state.show
-                ),
-                React.createElement(
-                    'p',
-                    null,
-                    this.state.toggle && this.state.title
-                )
-            );
-        }
-    }]);
-
-    return VisibilityToggle;
-}(React.Component);
-
-;
-
-ReactDOM.render(React.createElement(VisibilityToggle, null), document.getElementById('app'));
-
-// Old JSX way
-// const details = {
-//     title: 'Hey! these are some details you can now see!!!',
-//     toggle: false
-// };
-
-// const toggleVisibility = (e) => {
-//     e.preventDefault();
-//     details.toggle = !details.toggle;
-//     render();
-// };
-
-// const render = () => {
-//     const JSX = (
-//         <div>
-//             <h1>Visibility Toggle</h1>
-//             <button onClick={toggleVisibility}>{details.toggle ? 'Hide Details' : 'Show Details'}</button>
-//             {details.toggle && <p>{details.title}</p>}
-//         </div>
-//     );
-
-//     ReactDOM.render(JSX, document.getElementById('app'));
-// };
-
-// render();
+console.log(me);
--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -1,69 +1,11 @@
+//class
 class Person {
-    constructor (name = 'Anonymous', age = 0) {
-        this.name = name; //this refers to the class instance
+    constructor(name, age) {
+        this.name = name;
         this.age = age;
-    };
+    }
+}
 
-    getGreeting() {
-        //return 'Hi!, I am ' + this.name + '!';
-        return `Hi!, I am ${this.name}!`;
-    };
+const me = new Person('James', 30);
 
-    getDescription() {
-        return `${this.name} is ${this.age} year(s) old.`;
-    };
-
-};
-
-// sub classes using extends to bring attritbues of person class
-class Student extends Person {
-    constructor(name, age, major) {
-        super(name, age); // this allows us to take the default constructor from person without needed to actually set them up.
-        this.major = major;
-    };
-
-    hasMajor() {
-        return !!this.major;
-    };
-
-    // overrides the person method
-    getDescription() {
-        let description = super.getDescription();
-        
-        if (this.hasMajor()) {
-            description += ` Their major is ${this.major}`;
-        }
-        return description;
-    };
-};
-
-class Traveler extends Person {
-    constructor (name, age, homeLocation) {
-        super(name, age);
-        this.homeLocation = homeLocation;
-    };
-
-    getGreeting() {
-        let greeting = super.getGreeting();
-
-        if (this.homeLocation) {
-            greeting += ` I'm visiting from ${this.homeLocation}`;
-        }
-
-        return greeting;
-    };
-};
-
-const me = new Student('James Finkelstein', 30, 'Computer Science');
 console.log(me);
-console.log(me.hasMajor());
-console.log(me.getDescription());
-
-const other = new Student();
-console.log(other);
-console.log(other.hasMajor());
-console.log(other.getDescription());
-
-//traveller test
-const trav = new Traveler('James Finkelstein', 30, 'Toronto');
-console.log(trav.getGreeting());
