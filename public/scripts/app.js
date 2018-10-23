e30f060b6c904d12c8ee1dedce8698616b16e8a0
James Finkelstein <jfinkelstein9@me.com>
2018-10-23 14:15:39 -0400

    update classes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,15 +1,50 @@
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 //class
-var Person = function Person(name, age) {
-    _classCallCheck(this, Person);
+var Person = function () {
+    function Person() {
+        var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'John Doe';
+        var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
 
-    this.name = name;
-    this.age = age;
-};
+        _classCallCheck(this, Person);
 
-var me = new Person('James', 30);
+        this.name = name;
+        this.age = age;
+    }
 
-console.log(me);
+    _createClass(Person, [{
+        key: 'getGreeting',
+        value: function getGreeting() {
+            return 'Hi, I am ' + this.name + '!';
+        }
+    }, {
+        key: 'getDescription',
+        value: function getDescription() {
+            return this.name + ' is ' + this.age + ' year(s) old';
+        }
+    }]);
+
+    return Person;
+}();
+
+;
+
+// old
+// function person(name, age) {
+//     this.name = name;
+//     this.age = age;
+// }
+
+// const you = new person('john', 25);
+
+var me = new Person('James Fink', 30);
+console.log(me.getGreeting());
+console.log(me.getDescription());
+
+var other = new Person();
+console.log(other.getGreeting());
+console.log(other.getDescription());
--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -1,11 +1,31 @@
 //class
 class Person {
-    constructor(name, age) {
-        this.name = name;
+    constructor(name = 'John Doe', age = 0) {
+        this.name = name; 
         this.age = age;
-    }
-}
+    };
 
-const me = new Person('James', 30);
+    getGreeting() {
+        return `Hi, I am ${this.name}!`;
+    };
 
-console.log(me);
+    getDescription() {
+        return `${this.name} is ${this.age} year(s) old`;
+    };
+};
+
+// old
+// function person(name, age) {
+//     this.name = name;
+//     this.age = age;
+// }
+
+// const you = new person('john', 25);
+
+const me = new Person('James Fink', 30);
+console.log(me.getGreeting());
+console.log(me.getDescription());
+
+const other = new Person();
+console.log(other.getGreeting());
+console.log(other.getDescription());
